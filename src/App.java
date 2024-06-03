737f3143e32c14c0038baf3b2edef3ca42698a9b
jho-my <ojedasnachezjhonatan@gmail.com>
2024-06-02 21:07:56 -0500

    ejemplo de condicionales

--- src/App.java
+++ src/App.java
@@ -1,7 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        System.out.println("Hola Mundo");
+        ejemplos();
+    }
+
+    public static void sentenciaCondicionales() {
         System.out.println("Condicionales en Java");
-        if (condición) {
+        boolean condicion = true;
+        int exprecion = 1;
+
+        if (condicion) {
             // expreciones a seguir
         } else if (condicion) {
             // expresiones a seguir
@@ -10,18 +18,28 @@ public class App {
         }
         System.out.println("Condicionales en Java");
         switch (exprecion) {
-            case caso1:
+            case 1:
                 // expresiones
                 break;
-            case caso2:
+            case 2:
                 // expresiones
                 break;
-            case caso3:
+            case 3:
                 // expresiones
                 break;
-            default: //cuando ninguno de los casos se cumple
+            default: // cuando ningun0 de los casos se cumple
                 // expresiones
                 break;
+        }
+    }
+
+    public static void ejemplos() {
+        float promedio = 6.5f;
+        System.out.println(promedio);
+        if (promedio >= 6.5) {
+            System.out.println("Felicitaciones, excelente promedio");
+        } else {
+            System.out.println("Necesitas esforzarte un poco mas");
         }
     }
 }
