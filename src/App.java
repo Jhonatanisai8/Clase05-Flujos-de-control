4d59b1c4b001fef655340c07d9d694bb007655f8
jho-my <ojedasnachezjhonatan@gmail.com>
2024-06-02 20:49:23 -0500

    sentencias condicionales

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Condicionales en Java");
+        if (condición) {
+            // expreciones a seguir
+        } else if (condicion) {
+            // expresiones a seguir
+        } else {
+            // expreciones a seguir
+        }
+        System.out.println("Condicionales en Java");
+        switch (exprecion) {
+            case caso1:
+                // expresiones
+                break;
+            case caso2:
+                // expresiones
+                break;
+            case caso3:
+                // expresiones
+                break;
+            default: //cuando ninguno de los casos se cumple
+                // expresiones
+                break;
+        }
     }
 }
