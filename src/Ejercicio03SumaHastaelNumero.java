e087345d840097a15be4f0374ca9d408a41018e7
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-04 21:38:58 -0500

    Ejerccio 02: dado un numero mostrar la suma desde el 0 hasta dicho numero ingresado

--- src/Ejercicio03SumaHastaelNumero.java
+++ src/Ejercicio03SumaHastaelNumero.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 /**
  * Ejercicio03SumaHastaelNumero
  */
 public class Ejercicio03SumaHastaelNumero {
+    static Scanner leer = new Scanner(System.in);
 
-    
+    public static void main(String[] args) {
+        int numero, suma = 0;
+        System.out.print("Ingrese un número: ");
+        numero = leer.nextInt();
+
+        for (int i = 0; i <= numero; i++) {
+            suma = suma + i;
+        }
+
+        System.out.println("La suma es: " + suma);
+    }
 }
