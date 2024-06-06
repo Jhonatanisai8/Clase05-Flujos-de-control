a4755e5739f59845d08ba5fd96b8cd764ca1ba21
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-06 14:38:52 -0500

    Ejercicio 05 nuevo triangulo de asteriscos

--- src/Ejercicio05TrianguloAsteristicos.java
+++ src/Ejercicio05TrianguloAsteristicos.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
 public class Ejercicio05TrianguloAsteristicos {
+    static Scanner leer = new Scanner(System.in);
+
     public static void main(String[] args) {
+        int alto;
+        System.out.print("Ingresa la altura del triángulo:");
+        alto = leer.nextInt();
+
+        int i, j;
+
+        // formamamos el triángulo
+        for (i = 1; i <= alto; i++) {
+            System.out.println();
+            for (j = 1; j <= i; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+        for (i = 1; i <= alto; i++) {
+            System.out.println();
+            for (j = 1; j <= alto - i; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void piramide() {
         int alto = 5;
         int i = 1;
         int j = 1;
@@ -41,10 +68,3 @@ public class Ejercicio05TrianguloAsteristicos {
         }
     }
 }
-
-/*
- *
- * *
- * * *
- * 
- */
