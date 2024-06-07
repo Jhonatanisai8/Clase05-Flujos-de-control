d078f4d2476fc4c7ee82e5a22f2e7b73e9fbbb1d
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-07 10:34:22 -0500

    sentencia do-While

--- src/SentenciaWhile.java
+++ src/SentenciaWhile.java
@@ -1,28 +1,38 @@
 
 public class SentenciaWhile {
     public static void main(String[] args) {
-        otroEjemploWhile();     
+        sentenciaDoWhile();
     }
 
     public static void sentenciaWhile() {
         System.out.println("Sentencia WHILE");
         // nuestro contador
         int i = 0;
-        while(i < 5){
+        while (i < 5) {
             System.out.println(i);
             i++;
         }
     }
 
-    public static void otroEjemploWhile(){
+    public static void otroEjemploWhile() {
         int i = 0;
         boolean prueba = true;
         while (prueba) {
             if (i == 7) {
                 prueba = false;
             }
-            System.out.println(" "+i);
+            System.out.println(" " + i);
             i++;
         }
     }
+
+    public static void sentenciaDoWhile() {
+        System.out.println("Sentencia Do-While");
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 10);
+    }
+    
 }
