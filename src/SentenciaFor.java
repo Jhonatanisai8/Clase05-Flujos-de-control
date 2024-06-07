0da3c3b837e3938ebe411d65a23c094275d2daea
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-07 09:15:00 -0500

    sentencia for de forma diferente

--- src/SentenciaFor.java
+++ src/SentenciaFor.java
@@ -1,6 +1,16 @@
 public class SentenciaFor {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
+        otroFormaDiferenteFor();
+    }
+
+    public static void otroFormaDiferenteFor() {
+        System.out.println("==OTRA FORMA DE IMPLEMENTACIÓN DEL BUBLE FOR==");
+        int i = 0;
+        for (;;) {
+            if (i > 5) {
+                break;
+            }
+            i++;
             System.out.println(i);
         }
     }
