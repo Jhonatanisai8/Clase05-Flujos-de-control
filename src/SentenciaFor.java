273fbb48f78536c4523bdce3e05e1d01732ea150
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-07 09:25:43 -0500

    Uso del 'continue' para iterar solo los numeros impares

--- src/SentenciaFor.java
+++ src/SentenciaFor.java
@@ -1,6 +1,6 @@
 public class SentenciaFor {
     public static void main(String[] args) {
-        forConDosVariables();
+        numerosImpares();
     }
 
     public static void otroFormaDiferenteFor() {
@@ -26,6 +26,18 @@ public class SentenciaFor {
         System.out.println("=Bucle for con dos variables=");
         for (int i = 1, j = 10; i < j; i++, j--) {
             System.out.println(i + " = " + j);
+        }
+    }
+
+    public static void numerosImpares() {
+        System.out.println("=Uso del 'continue' para iterar solo los numeros impares=");
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                // esta sentencia permite salir de la iteracion
+                // cada ves que sea par se sale
+                continue;
+            }
+            System.out.println(i);
         }
     }
 }
