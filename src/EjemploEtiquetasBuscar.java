fd8b1daa8e0566eb0eb48ea2707d537717198bfa
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-07 11:12:37 -0500

    Encontrado una palabra con etiquetas

--- src/EjemploEtiquetasBuscar.java
+++ src/EjemploEtiquetasBuscar.java
@@ -1,6 +1,6 @@
 public class EjemploEtiquetasBuscar {
     public static void main(String[] args) {
-        encontrarCaracter();
+        encontrarPalabra();
     }
 
     public static void encontrarCaracter() {
@@ -16,6 +16,28 @@ public class EjemploEtiquetasBuscar {
             }
             contador++;
         }
-        System.out.println("Encontrado = "+contador+" veces el caracter t ");
+        System.out.println("Encontrado = " + contador + " veces el caracter t ");
     }
+
+    public static void encontrarPalabra() {
+        System.out.println("=Encontrado una palabra con etiquetas=\n");
+        String frase = "Pablito clavó un clavito en la calva de un calvito. En la calva de un calvito, un clavito clavó Pablito. Pablito clavó un clavito. ¿Qué clavito clavó Pablito?.";
+        System.out.println(frase);
+        String palabra = "clavito";
+        int maxPalabra = palabra.length();
+        // recorremos la frase
+        int tamanio = frase.length();
+        int contador = 0;
+        bucle1: for (int i = 0; i < tamanio; i++) {
+            int k = i;// variable auxiliar
+            for (int j = 0; j < maxPalabra; j++) {
+                if (frase.charAt(k++) != palabra.charAt(j)) {
+                    continue bucle1;
+                }
+            }
+            contador++;
+        }
+        System.out.println("Encontrado = " + contador + " veces la palabra " + palabra);
+    }
+
 }
