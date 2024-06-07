dbbb8581f09d7f4ccb168e6de03de5a76342b87a
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-07 08:53:40 -0500

    Sentencia For

--- src/cuadrado.py
+++
@@ -1,9 +0,0 @@
-i = int()
-j = int()
-alto = int()
-print("Ingrese la longitud de los lados: ")
-alto = int(input())
-for i in range(1,alto+1):
-	for j in range(1,alto+1):
-		print(" * ", end="")
-	print("")
