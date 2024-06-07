17e7f6a99d89d5141cf747215502196d79fbe5e6
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-07 09:36:39 -0500

    iterando sobre un arreglo haciendo el uso del metodo conteins

--- src/SentenciaForArreglo.java
+++ src/SentenciaForArreglo.java
@@ -1,7 +1,7 @@
 public class SentenciaForArreglo {
     public static void main(String[] args) {
         System.out.println("SentenciaForArreglo.main()");
-        iterandoArreglos();
+        usoContainsAlIterarArreglo();
     }
 
     public static void iterandoArreglos() {
@@ -10,11 +10,22 @@ public class SentenciaForArreglo {
         int longitud = nombres.length;
         for (int i = 0; i < longitud; i++) {
             if (nombres[i].equalsIgnoreCase("juan")) {
-                continue;//omite el valor de juan
+                continue;// omite el valor de juan
             }
-            System.out.println(i+" "+nombres[i]);
+            System.out.println(i + " " + nombres[i]);
         }
     }
 
-  
+    public static void usoContainsAlIterarArreglo() {
+        System.out.println("usoContainsAlIterarArreglo()");
+        String nombres[] = { "Maria", "Pedro", "Juan", "Pedro", "Walter", "Juanito", "Carlos" };
+        int longitud = nombres.length;
+        for (int i = 0; i < longitud; i++) {
+            if (nombres[i].contains("Juan")
+                    || nombres[i].contains("Maria")) {
+                continue;// omite el valor de juan
+            }
+            System.out.println(i + " " + nombres[i]);
+        }
+    }
 }
