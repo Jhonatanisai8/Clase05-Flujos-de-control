d57a289df05939c4f794bddb8b89cb7b5c3fb21b
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-07 10:58:59 -0500

    Ejemplo de bucle aninados con etiquetas

--- src/SentenciasBucleEtiquetas.java
+++ src/SentenciasBucleEtiquetas.java
@@ -1,6 +1,6 @@
 public class SentenciasBucleEtiquetas {
     public static void main(String[] args) {
-        ejemploAninadado();
+        ejemploAninadadoDiasSemana();
     }
 
     public static void ejemploAninadado() {
@@ -22,6 +22,31 @@ public class SentenciasBucleEtiquetas {
                 continue;
             }
             System.out.println(j);
+        }
+    }
+
+    public static void ejemploAninadadoBreak() {
+        bucle1: for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+
+                if (i == 2) {// omite el número dos el bucle iterabl con i
+                    break bucle1;
+                }
+                System.out.println("i= " + i + " j= " + j);
+            }
+        }
+    }
+
+    public static void ejemploAninadadoDiasSemana() {
+        bucle1: for (int i = 1; i <= 7; i++) {
+            for (int j = 1; j <= 7; j++) {
+
+                if (i == 6 || i == 7) {// omite el número dos el bucle iterabl con i
+                    System.out.println("Dia: " + i + " descando fin de semana");
+                    continue bucle1;
+                }
+                System.out.println("Dia " + i + " trabajando " + j + " Horas");
+            }
         }
     }
 }
